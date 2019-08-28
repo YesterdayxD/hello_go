50995a9f230fd3e5a130128439ad2bd309a96512
limk <1144650984@qq.com>
2019-08-28 15:07:39 +0800

    判断一种数据类型是否实现了某个接口的技巧

---
+++ aboutInterface/aboutInterface.go
@@ -0,0 +1,33 @@
+package aboutInterface
+
+//定义一种借口类型
+type AKindOfInterface interface {
+	FunctionA() string
+	FunctionB() string
+}
+
+//定义一个结构体
+type AKindOfStrut struct {
+	AttributionA int
+	AttributionB string
+}
+
+//在结构体AKindOfStrut实现AKindOfInterface借口的函数（所有的函数）
+func (s *AKindOfStrut) FunctionA() string {
+	//fmt.Println("hello")
+	return "hello,A"
+}
+
+func (s *AKindOfStrut) FunctionB() string {
+	//fmt.Println("hello")
+	return "hello,B"
+}
+
+func main() {
+	//通过以下语法判断某个数据类型是否全部了所有的借口
+	//如果没有实现，则会编译失败
+	// 	type T struct{}
+	//  var _ I = &T{}
+	//其中 T 为结构体（数据类型），I为接口
+	var _ AKindOfInterface = &AKindOfStrut{}
+}
